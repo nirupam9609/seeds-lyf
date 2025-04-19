104168271f6e3973fcceeb776c9a1d3757be797d
Nirupam Samanta <nirupamsamanta9609@gmail.com>
2025-04-19 11:54:30 +0530

    push

---
+++ script.js
@@ -0,0 +1,182 @@
+const productList = [
+    {
+        name: "PANEER",
+        price: 50,
+        unit: "100GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.paneer123&pid=Api&P=0&h=180",
+        type: "dairy",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "MILK",
+        price: 25,
+        unit: "500ML",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.milk123&pid=Api&P=0&h=180",
+        type: "dairy",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "CHEESE",
+        price: 80,
+        unit: "200GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.cheese123&pid=Api&P=0&h=180",
+        type: "dairy",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "YOGURT",
+        price: 20,
+        unit: "100GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.yogurt123&pid=Api&P=0&h=180",
+        type: "dairy",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "APPLE",
+        price: 120,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.apple123&pid=Api&P=0&h=180",
+        type: "fruit",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "BANANA",
+        price: 40,
+        unit: "1DOZEN",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.banana123&pid=Api&P=0&h=180",
+        type: "fruit",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "MANGO",
+        price: 150,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.mango123&pid=Api&P=0&h=180",
+        type: "fruit",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "ORANGE",
+        price: 90,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.orange123&pid=Api&P=0&h=180",
+        type: "fruit",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "TOMATO",
+        price: 30,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.tomato123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "POTATO",
+        price: 25,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.potato123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "ONION",
+        price: 28,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.onion123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "CARROT",
+        price: 35,
+        unit: "500GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.carrot123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "CUCUMBER",
+        price: 32,
+        unit: "500GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.cucumber123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "PEAS",
+        price: 60,
+        unit: "500GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.peas123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "GRAPES",
+        price: 100,
+        unit: "500GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.grapes123&pid=Api&P=0&h=180",
+        type: "fruit",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "LEMON",
+        price: 20,
+        unit: "250GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.lemon123&pid=Api&P=0&h=180",
+        type: "fruit",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "CHICKEN",
+        price: 220,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.chicken123&pid=Api&P=0&h=180",
+        type: "nonveg",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "MUTTON",
+        price: 450,
+        unit: "1KG",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.mutton123&pid=Api&P=0&h=180",
+        type: "nonveg",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "EGG",
+        price: 60,
+        unit: "1DOZEN",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.egg123&pid=Api&P=0&h=180",
+        type: "nonveg",
+        buttonText: "Add to Cart"
+    },
+    {
+        name: "GINGER",
+        price: 75,
+        unit: "250GM",
+        currency: "RS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.ginger123&pid=Api&P=0&h=180",
+        type: "vegetable",
+        buttonText: "Add to Cart"
+    }
+];
